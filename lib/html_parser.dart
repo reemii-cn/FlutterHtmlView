454880564cf7bd3beb86679bc03452bbd7746027
hyccpq <hyccpq@hotmail.com>
2019-09-03 15:57:04 +0800

    12

--- lib/html_parser.dart
+++ lib/html_parser.dart
@@ -23,10 +23,11 @@ class HtmlParser {
       var src = e.attributes['src'];
 
       if (src.startsWith("http") || src.startsWith("https")) {
-        widgetList.add(new CachedNetworkImage(
+        widgetList.add(Center(
+            child: CachedNetworkImage(
           imageUrl: src,
           fit: BoxFit.scaleDown,
-        ));
+        )));
       } else if (src.startsWith('data:image')) {
         var exp = new RegExp(r'data:.*;base64,');
         var base64Str = src.replaceAll(exp, '');
